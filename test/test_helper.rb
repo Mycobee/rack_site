42411827bbcd09af496f302ad8fb588d49659750
Rob Stringer <41843577+Mycobee@users.noreply.github.com>
2019-04-27 11:46:21 -0600

    Created index page

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,10 @@ require 'capybara/minitest'
 require './app/controllers/rack_site'
 
 Capybara.app = RackSite
+Capybara.save_path = './tmp/capybara'
 
 class CapybaraTestCase < Minitest::Test
   include Capybara::DSL
   include Capybara::Minitest::Assertions
 end
+
